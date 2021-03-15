56322d934e6d2f2044d25111cef416430ca346ff
Cooper Halvordson <cjhalvord@gmail.com>
2021-03-15 12:41:07 -0400

    Added events and locations for legendary pokemon. Mew, Mewtwo, Celebii.

--- maps/RedsHouse2F.asm
+++ maps/RedsHouse2F.asm
@@ -1,7 +1,45 @@
+	object_const_def
+	const RED_HOUSE_MEW
+
 RedsHouse2F_MapScripts:
 	def_scene_scripts
 
 	def_callbacks
+	callback MAPCALLBACK_OBJECTS, .Mew
+
+.Mew:
+	checkevent EVENT_FOUGHT_MEW
+	iftrue .NoAppear
+	checkevent EVENT_BEAT_RED
+	iftrue .Appear
+	sjump .NoAppear
+
+.Appear:
+	appear RED_HOUSE_MEW
+	endcallback
+
+.NoAppear:
+	disappear RED_HOUSE_MEW
+	endcallback
+
+Mew:
+	faceplayer
+	opentext
+	writetext MEWText
+	cry MEW
+	pause 15
+	closetext
+	setevent EVENT_FOUGHT_MEW
+	loadvar VAR_BATTLETYPE, BATTLETYPE_FORCEITEM
+	loadwildmon MEW, 70
+	startbattle
+	disappear RED_HOUSE_MEW
+	reloadmapafterbattle
+	end
+
+MewText:
+	text "Mewww"
+	done
 
 RedsHouse2FN64Script:
 	jumptext RedsHouse2FN64Text
@@ -36,3 +74,4 @@ RedsHouse2F_MapEvents:
 	bg_event  0,  1, BGEVENT_READ, RedsHouse2FPCScript
 
 	def_object_events
+	object_event  2,  4, SPRITE_JYNX, SPRITEMOVEDATA_POKEMON, 0, 0 , -1, -1, 0, OBJECTTYPE_SCRIPT, 0, Mew, EVENT_RED_HOUSE_MEW
