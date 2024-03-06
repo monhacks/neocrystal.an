d46c491577637d816cc1c06a9f973c0eae33fc43
Adam Novitch <adam.novitch@gmail.com>
2024-03-06 19:28:50 +0000

    Update Route 29 and 46 mons

--- data/wild/johto_grass.asm
+++ data/wild/johto_grass.asm
@@ -1239,27 +1239,27 @@ JohtoGrassWildMons:
 	; morn
 	db 2, PIDGEY
 	db 2, SENTRET
-	db 3, PIDGEY
-	db 3, SENTRET
 	db 2, RATTATA
-	db 3, HOPPIP
-	db 3, HOPPIP
+	db 2, HOPPIP
+	db 2, CATERPIE
+	db 2, SPEAROW
+	db 2, IGGLYBUFF
 	; day
 	db 2, PIDGEY
 	db 2, SENTRET
-	db 3, PIDGEY
-	db 3, SENTRET
 	db 2, RATTATA
-	db 3, HOPPIP
-	db 3, HOPPIP
+	db 2, HOPPIP
+	db 2, LEDYBA
+	db 2, SPEAROW
+	db 2, PICHU
 	; nite
 	db 2, HOOTHOOT
 	db 2, RATTATA
-	db 3, HOOTHOOT
-	db 3, RATTATA
-	db 2, RATTATA
-	db 3, HOOTHOOT
-	db 3, HOOTHOOT
+	db 2, CATERPIE
+	db 2, SPINARAK
+	db 2, ODDISH
+	db 2, IGGLYBUFF
+	db 2, CLEFFA
 	end_grass_wildmons
 
 	def_grass_wildmons ROUTE_30
@@ -1657,29 +1657,29 @@ JohtoGrassWildMons:
 	def_grass_wildmons ROUTE_46
 	db 10 percent, 10 percent, 10 percent ; encounter rates: morn/day/nite
 	; morn
-	db 2, GEODUDE
+	db 2, RATTATA
 	db 2, SPEAROW
-	db 3, GEODUDE
-	db 3, RATTATA
+	db 2, GEODUDE
+	db 2, HOPPIP
+	db 2, LEDYBA
+	db 2, IGGLYBUFF
 	db 2, PHANPY
-	db 2, RATTATA
-	db 2, RATTATA
 	; day
-	db 2, GEODUDE
+	db 2, RATTATA
 	db 2, SPEAROW
-	db 3, GEODUDE
-	db 3, RATTATA
-	db 2, RATTATA
-	db 2, RATTATA
-	db 2, RATTATA
-	; nite
 	db 2, GEODUDE
+	db 2, HOPPIP
+	db 2, LEDYBA
+	db 2, IGGLYBUFF
+	db 2, PHANPY
+	; nite
 	db 2, RATTATA
-	db 3, GEODUDE
-	db 3, RATTATA
-	db 2, RATTATA
-	db 2, RATTATA
-	db 2, RATTATA
+	db 2, HOOTHOOT
+	db 2, GEODUDE
+	db 2, SPINARAK
+	db 2, ODDISH
+	db 2, IGGLYBUFF
+	db 2, CLEFFA
 	end_grass_wildmons
 
 	def_grass_wildmons SILVER_CAVE_OUTSIDE
