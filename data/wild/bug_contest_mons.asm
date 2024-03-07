116dd5b92b7858eff7f1548db80fc76d4451b285
Adam Novitch <adam.novitch@gmail.com>
2024-03-07 19:03:32 +0000

    Add bug contest mons

--- data/wild/bug_contest_mons.asm
+++ data/wild/bug_contest_mons.asm
@@ -1,17 +1,18 @@
 ContestMons:
 	;   %, species,   min, max
-	db 10, CATERPIE,    7, 18
-	db 10, WEEDLE,      7, 18
-	db  9, LEDYBA,     10, 16
-	db  9, VENONAT,    10, 16
-	db  9, PARAS,      10, 16
-	db  7, METAPOD,     9, 18
-	db  7, KAKUNA,      9, 18
-	db  7, YANMA,      10, 16
-	db  7, PINECO,     10, 16
-	db  5, BUTTERFREE, 12, 15
-	db  5, BEEDRILL,   12, 15
-	db  5, HERACROSS,  13, 14
-	db  5, SCYTHER,    13, 14
-	db  5, PINSIR,     13, 14
+	db  9, CATERPIE,   13, 20
+	db  9, WEEDLE,     13, 20
+	db  8, LEDYBA,     13, 20
+	db  8, VENONAT,    13, 20
+	db  8, PARAS,      13, 20
+	db  7, METAPOD,    13, 27
+	db  7, KAKUNA,     13, 27
+	db  7, YANMA,      13, 17
+	db  7, PINECO,     13, 17
+	db  5, LEDIAN,     13, 16
+	db  5, BUTTERFREE, 13, 16
+	db  5, BEEDRILL,   13, 16
+	db  5, HERACROSS,  13, 15
+	db  5, SCYTHER,    13, 15
+	db  5, PINSIR,     13, 15
 	db -1, VENOMOTH,   30, 40
