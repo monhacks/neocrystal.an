76bfec3b226fb9f388b563d4c9f6a422fcbcabef
Adam Novitch <adam.novitch@gmail.com>
2024-07-05 17:41:20 +0000

    Update catching contest levels

--- data/wild/bug_contest_mons.asm
+++ data/wild/bug_contest_mons.asm
@@ -1,18 +1,18 @@
 ContestMons:
 	;   %, species,   min, max
-	db  9, CATERPIE,   13, 20
-	db  9, WEEDLE,     13, 20
-	db  8, LEDYBA,     13, 20
-	db  8, VENONAT,    13, 20
-	db  8, PARAS,      13, 20
-	db  7, METAPOD,    13, 27
-	db  7, KAKUNA,     13, 27
+	db  9, CATERPIE,   13, 19
+	db  9, WEEDLE,     13, 19
+	db  8, LEDYBA,     13, 18
+	db  8, VENONAT,    13, 18
+	db  8, PARAS,      13, 18
+	db  7, METAPOD,    13, 18
+	db  7, KAKUNA,     13, 18
 	db  7, YANMA,      13, 17
 	db  7, PINECO,     13, 17
 	db  5, LEDIAN,     13, 16
-	db  5, BUTTERFREE, 13, 16
-	db  5, BEEDRILL,   13, 16
-	db  5, HERACROSS,  13, 15
-	db  5, SCYTHER,    13, 15
-	db  5, PINSIR,     13, 15
+	db  5, BUTTERFREE, 12, 16
+	db  5, BEEDRILL,   12, 16
+	db  5, HERACROSS,  12, 15
+	db  5, SCYTHER,    12, 15
+	db  5, PINSIR,     12, 15
 	db -1, VENOMOTH,   30, 40
